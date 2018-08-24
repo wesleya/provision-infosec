e35fe28ecf6da3d7f74cfebbed88b7aa5399f6e0
wagena <wagena@domaintools.com>
2018-08-24 09:44:40 -0700

    move webgoat script into it's own repo

--- storage/scripts/webgoat.sh
+++ storage/scripts/webgoat.sh
@@ -1,31 +1,7 @@
 #!/bin/bash
 
-# install software
-apt-get update
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get install -y docker-ce
-DEBIAN_FRONTEND=noninteractive apt-get install -y iptables-persistent
+# clone repo with startup scripts
+git clone https://github.com/wesleya/webgoat.git
 
-# apt-get adds this to init.d/ so it will restart on boot
-docker pull webgoat/webgoat-8.0
-
-# secure box
-iptables -I INPUT 1 -s 63.150.103.146 -j ACCEPT
-iptables -I INPUT 2 -j DROP
-iptables -I OUTPUT 1 -d 63.150.103.146 -j ACCEPT
-iptables -I OUTPUT 2 -j DROP
-iptables -I FORWARD 1 -s 63.150.103.146 -j ACCEPT
-iptables -I FORWARD 2 -d 63.150.103.146 -j ACCEPT
-iptables -I FORWARD 3 -j DROP
-iptables --policy INPUT DROP
-iptables --policy OUTPUT DROP
-iptables --policy FORWARD DROP
-iptables-save > /etc/iptables/rules.v4
-ip6tables-save > /etc/iptables/rules.v6
-
-# use the --restart always so this container auto-runs everytime docker starts
-# which combined with the init.d command above should mean container runs on every reboot
-docker run --restart always -p 80:8080 webgoat/webgoat-8.0 /home/webgoat/start.sh
+# run init script with access ip variable
+source webgoat/init.sh "63.150.103.146" >> init.log
